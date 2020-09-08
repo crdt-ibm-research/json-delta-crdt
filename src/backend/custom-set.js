de06b8aff2d237c73c0be8dbc38a1f71e0933d46
guykhazma <Guy.Khazma@ibm.com>
2020-09-08 13:09:58 +0300

    initial commit

---
+++ src/backend/custom-set.js
@@ -0,0 +1,38 @@
+'use strict'
+
+module.exports = class CustomSet {
+  constructor () {
+    this._refs = new Map()
+  }
+
+  static from(base) {
+    const result =  new CustomSet()
+    result._refs = new Map([...base._refs])
+    return result
+  }
+
+  add (o) {
+    const key = keyFor(o)
+    if (!this._refs.has(key)) {
+      this._refs.set(key, o)
+    }
+  }
+
+  delete (o) {
+    const key = keyFor(o)
+    this._refs.delete(key)
+  }
+
+  has (o) {
+    const key = keyFor(o)
+    return this._refs.has(key)
+  }
+
+  values () {
+    return this._refs.values()
+  }
+}
+
+function keyFor (o) {
+  return JSON.stringify(o)
+}
