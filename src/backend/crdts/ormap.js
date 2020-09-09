fbdb0a931c149f14ae37be5008b09e58a905f53f
Arik Dov Rinberg <arik.rinberg@ibm.com>
2020-09-09 14:42:29 +0300

    Added deleting of other types

--- src/backend/crdts/ormap.js
+++ src/backend/crdts/ormap.js
@@ -8,104 +8,127 @@ const MVReg = require('./mvreg')
 const { ALIVE, MAP, ARRAY, VALUE } = require('../constants')
 
 class ORMap {
-    static typename() {
-        return "or-map"
-    }
+	static typename() {
+		return "or-map"
+	}
 
-    static value([m, cc]) {
-        assert(m instanceof DotMap)
+	static value([m, cc]) {
+		assert(m instanceof DotMap)
 
-        let retMap = {}
-        for (let [key, value] of m.state.entries()) {
-            if (key === ALIVE) continue
-            const innerMap = m.get(key)
-            if (innerMap.has(MAP)) {
-                value = ORMap.value([innerMap.get(MAP), cc])
-            } else if (innerMap.has(ARRAY)) {
-                value = ORArray.values([innerMap.get(ARRAY), cc])
-            } else {
-                value = MVReg.values([innerMap.get(VALUE), cc])
-            }
+		let retMap = {}
+		for (let [key, value] of m.state.entries()) {
+			if (key === ALIVE) continue
+			const innerMap = m.get(key)
+			if (innerMap.has(MAP)) {
+				value = ORMap.value([innerMap.get(MAP), cc])
+			} else if (innerMap.has(ARRAY)) {
+				value = ORArray.values([innerMap.get(ARRAY), cc])
+			} else {
+				value = MVReg.values([innerMap.get(VALUE), cc])
+			}
 
-            retMap[key] = value
-        }
-        return retMap
-    }
+			retMap[key] = value
+		}
+		return retMap
+	}
 
-    static create([m,cc]) {
-        
-        m = m || new DotMap(ORMap.typename())
+	static create([m,cc]) {
+		m = m || new DotMap(ORMap.typename())
 
-        assert(m instanceof DotMap)
-        assert(cc instanceof CausalContext)
-        
-        const [retFun, retCC] = MVReg.write(true, [m.get(ALIVE), cc])
-        const retDotMap = new DotMap(ORMap.typename(), new Map().set(ALIVE, retFun))
+		assert(m instanceof DotMap)
+		assert(cc instanceof CausalContext)
+		
+		const [retFun, retCC] = MVReg.write(true, [m.get(ALIVE), cc])
+		const retDotMap = new DotMap(ORMap.typename(), new Map().set(ALIVE, retFun))
 
-        return [retDotMap, retCC]
-    }
+		return [retDotMap, retCC]
+	}
 
-    static applyToMap(o, k, [m,cc]) {
-        // TODO: Delete other two
-        const inner = function ([m,cc]) {return ORMap.apply(o, MAP, [m, cc])}
-        return ORMap.apply(inner, k, [m,cc])
-    }
+	static applyToMap(o, k, [m,cc]) {
+		const inner = function ([m,cc]) {return ORMap.apply(o, MAP, [m, cc])}
+		const [retMap, retCC] = ORMap.apply(inner, k, [m,cc])
 
-    static applyToArray(o, k, [m,cc]) {
-        // TODO: Delete other two
-        const inner = function ([m,cc]) {return ORMap.apply(o, ARRAY, [m, cc])}
-        return ORMap.apply(inner, k, [m,cc])
-    }
+		if (m.get(ARRAY)) {
+			retCC.insertDots(m.get(ARRAY).dots())
+		}
+		if (m.get(VALUE)) {
+			retCC.insertDots(m.get(VALUE).dots())
+		}
 
-    static applyToValue(o, k, [m,cc]) {
-        // TODO: Delete other two
-        const inner = function ([m,cc]) {return ORMap.apply(o, VALUE, [m, cc])}
-        return ORMap.apply(inner, k, [m,cc])
-    }
+		return [retMap, retCC]
+	}
 
-    static apply(o, k, [m,cc]) {
-        m = m || new DotMap(ORMap.typename())
+	static applyToArray(o, k, [m,cc]) {
+		const inner = function ([m,cc]) {return ORMap.apply(o, ARRAY, [m, cc])}
+		const [retMap, retCC] = ORMap.apply(inner, k, [m,cc])
+		
+		if (m.get(MAP)) {
+			retCC.insertDots(m.get(MAP).dots())
+		}
+		if (m.get(VALUE)) {
+			retCC.insertDots(m.get(VALUE).dots())
+		}
 
-        assert(m instanceof DotMap)
-        assert(m.typename === ORMap.typename())
-        assert(cc instanceof CausalContext)
+		return [retMap, retCC]
+	}
 
-        const retDotMap = new DotMap(ORMap.typename())
+	static applyToValue(o, k, [m,cc]) {
+		const inner = function ([m,cc]) {return ORMap.apply(o, VALUE, [m, cc])}
+		const [retMap, retCC] = ORMap.apply(inner, k, [m,cc])
+		
+		if (m.get(MAP)) {
+			retCC.insertDots(m.get(MAP).dots())
+		}
+		if (m.get(ARRAY)) {
+			retCC.insertDots(m.get(ARRAY).dots())
+		}
 
-        // First add ALIVE
-        const [fun, funCC] = MVReg.write(true, [m.get(ALIVE), cc])
-        retDotMap.set(ALIVE, fun)
+		return [retMap, retCC]
+	}
 
-        // Next call o (don't forget to add the dot to the CC)
-        const [newV, retCC] = o([m.get(k), cc.join(funCC)])
-        retDotMap.set(k ,newV)
+	static apply(o, k, [m,cc]) {
+		m = m || new DotMap(ORMap.typename())
 
-        return [retDotMap, retCC.join(funCC)]
-    }
+		assert(m instanceof DotMap)
+		assert(m.typename === ORMap.typename())
+		assert(cc instanceof CausalContext)
 
-    static remove(k, [m,cc]) {
-        assert(m instanceof DotMap)
-        assert(cc instanceof CausalContext)
+		const retDotMap = new DotMap(ORMap.typename())
 
-        const retCC = new CausalContext().insertDots(m.get(k).dots())
-        return [new DotMap(ORMap.typename()), retCC]
-    }
+		// First add ALIVE
+		const [fun, funCC] = MVReg.write(true, [m.get(ALIVE), cc])
+		retDotMap.set(ALIVE, fun)
 
-    static clear([m,cc]) {        
-        assert(m instanceof DotMap)
-        assert(cc instanceof CausalContext)
+		// Next call o (don't forget to add the dot to the CC)
+		const [newV, retCC] = o([m.get(k), cc.join(funCC)])
+		retDotMap.set(k ,newV)
 
-        const retDotMap = new DotMap(ORMap.typename())
+		return [retDotMap, retCC.join(funCC)]
+	}
 
-        // First write ALIVE
-        const [fun, funCC] = MVReg.write(true, [m.get(ALIVE), cc])
-        retDotMap.set(ALIVE, fun)
+	static remove(k, [m,cc]) {
+		assert(m instanceof DotMap)
+		assert(cc instanceof CausalContext)
 
-        // Next remove all dots
-        const retCC = funCC.insertDots(m.dots())
+		const retCC = new CausalContext().insertDots(m.get(k).dots())
+		return [new DotMap(ORMap.typename()), retCC]
+	}
 
-        return [retDotMap, retCC]
-    }
+	static clear([m,cc]) {        
+		assert(m instanceof DotMap)
+		assert(cc instanceof CausalContext)
+
+		const retDotMap = new DotMap(ORMap.typename())
+
+		// First write ALIVE
+		const [fun, funCC] = MVReg.write(true, [m.get(ALIVE), cc])
+		retDotMap.set(ALIVE, fun)
+
+		// Next remove all dots
+		const retCC = funCC.insertDots(m.dots())
+
+		return [retDotMap, retCC]
+	}
 }
 
 module.exports = ORMap
