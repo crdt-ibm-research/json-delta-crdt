3e0bdda31f1a3367ab6cebd2a9a1ec2452292165
Arik Dov Rinberg <arik.rinberg@ibm.com>
2020-09-09 14:59:49 +0300

    Added skeleton for ORArray

--- src/backend/crdts/orarray.js
+++ src/backend/crdts/orarray.js
@@ -18,77 +18,26 @@ class ORArray {
 
     static create([m,cc]) {
         
-        m = m || new DotMap(ORMap.typename())
-
-        assert(m instanceof DotMap)
-        assert(cc instanceof CausalContext)
-        
-        const [retFun, retCC] = MVReg.write(true, [m.get(ALIVE), cc])
-        const retDotMap = new DotMap(ORMap.typename(), new Map().set(ALIVE, retFun))
-
-        return [retDotMap, retCC]
     }
 
-    static applyToMap(o, k, [m,cc]) {
-        // TODO: Delete other two
-        const inner = function ([m,cc]) {return ORMap.apply(o, MAP, [m, cc])}
-        return ORMap.apply(inner, k, [m,cc])
+    static insert(uid, o, p, [m,cc]) {
+
     }
 
-    static applyToArray(o, k, [m,cc]) {
-        // TODO: Delete other two
-        const inner = function ([m,cc]) {return ORMap.apply(o, ARRAY, [m, cc])}
-        return ORMap.apply(inner, k, [m,cc])
+    static apply(uid, o, p, [m,cc]) {
+
     }
 
-    static applyToValue(o, k, [m,cc]) {
-        // TODO: Delete other two
-        const inner = function ([m,cc]) {return ORMap.apply(o, VALUE, [m, cc])}
-        return ORMap.apply(inner, k, [m,cc])
+    static move(uid, p, [m,cc]) {
+
     }
 
-    static apply(o, k, [m,cc]) {
-        m = m || new DotMap(ORMap.typename())
+    static delete(uid, [m,cc]) {
 
-        assert(m instanceof DotMap)
-        assert(m.typename === ORMap.typename())
-        assert(cc instanceof CausalContext)
-
-        const retDotMap = new DotMap(ORMap.typename())
-
-        // First add ALIVE
-        const [fun, funCC] = MVReg.write(true, [m.get(ALIVE), cc])
-        retDotMap.set(ALIVE, fun)
-
-        // Next call o (don't forget to add the dot to the CC)
-        const [newV, retCC] = o([m.get(k), cc.join(funCC)])
-        retDotMap.set(k ,newV)
-
-        return [retDotMap, retCC.join(funCC)]
-    }
-
-    static remove(k, [m,cc]) {
-        assert(m instanceof DotMap)
-        assert(cc instanceof CausalContext)
-
-        const retCC = new CausalContext().insertDots(m.get(k).dots())
-        return [new DotMap(ORMap.typename()), retCC]
     }
 
     static clear([m,cc]) {        
-        assert(m instanceof DotMap)
-        assert(cc instanceof CausalContext)
 
-        const retDotMap = new DotMap(ORMap.typename())
-
-        // First write ALIVE
-        const [fun, funCC] = MVReg.write(true, [m.get(ALIVE), cc])
-        retDotMap.set(ALIVE, fun)
-
-        // Next remove all dots
-        const retCC = funCC.insertDots(m.dots())
-
-        return [retDotMap, retCC]
     }
 }
 
