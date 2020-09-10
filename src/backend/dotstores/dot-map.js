b3054304250364b6c9501e7d2e1e9babd3b392a1
guykhazma <Guy.Khazma@ibm.com>
2020-09-10 14:06:21 +0300

    improve backend

--- src/backend/dotstores/dot-map.js
+++ src/backend/dotstores/dot-map.js
@@ -11,6 +11,10 @@ class DotMap {
 		this.state = state || new Map() // {K: Any => V: DotStore}
 	}
 
+	static from(other) {
+		return new DotMap(other.typename, new Map([...other.state]))
+	}
+
 	dots() { // returns Set()
 		const result = new Set()
 		for (let value of this.state.values()) {
@@ -20,63 +24,63 @@ class DotMap {
 		return result
 	}
 
-  isBottom() {
-    return this.state.size === 0
+	isBottom() {
+		return this.state.size === 0
 	}
-	
+
 	size() {
-    return this.state.size
-  }
+		return this.state.size
+	}
 
-  has(key) {
-    return this.state.has(key)
-  }
+	has(key) {
+		return this.state.has(key)
+	}
 
-  get(key) {
-    return this.state.get(key)
-  }
+	get(key) {
+		return this.state.get(key)
+	}
 
-  set(key, value) {
-	return this.state.set(key, value)
-	this
-  }
+	set(key, value) {
+		return this.state.set(key, value)
+		this
+	}
 
 	// returns the join
-  static join([m1, cc1], [m2 ,cc2]) {
-	m1 = m1 || new DotMap()
-	m2 = m2 || new DotMap()
+	static join([m1, cc1], [m2, cc2]) {
+		m1 = m1 || new DotMap()
+		m2 = m2 || new DotMap()
 
-	assert(m1 instanceof DotMap && m2 instanceof DotMap)
-	assert(cc1 instanceof CausalContext && cc2 instanceof CausalContext)
+		assert(m1 instanceof DotMap && m2 instanceof DotMap)
+		assert(cc1 instanceof CausalContext && cc2 instanceof CausalContext)
 
-	const resCC = CausalContext.from(cc1).join(cc2)
+		const resCC = CausalContext.from(cc1).join(cc2)
 
-	const resM = new DotMap(m1.typename || m2.typename)
+		const resM = new DotMap(m1.typename || m2.typename)
 
-	const allKeys = new Set([...m1.state.keys(), ...m2.state.keys()])
-	for (let key of allKeys) {
-		let left = m1.get(key) // DotStore
-		let right = m2.get(key) // DotStore
+		const allKeys = new Set([...m1.state.keys(), ...m2.state.keys()])
+		for (let key of allKeys) {
+			let left = m1.get(key) // DotStore
+			let right = m2.get(key) // DotStore
 
-		const type = left || right
-		let joinFunction;
-		if (type instanceof DotMap) {
-			joinFunction = DotMap.join
-		}	else if (type instanceof DotFunMap) {
-			joinFunction = DotFunMap.join
-		} else {
-			joinFunction = DotFun.join
+			const type = left || right
+			let joinFunction;
+			if (type instanceof DotMap) {
+				joinFunction = DotMap.join
+			} else if (type instanceof DotFunMap) {
+				joinFunction = DotFunMap.join
+			} else {
+				joinFunction = DotFun.join
+			}
+
+			const res = joinFunction([left, cc1], [right, cc2])
+			const valueForKey = res[0]
+
+			if (!valueForKey.isBottom()) {
+				resM.state.set(key, valueForKey)
+			}
 		}
 
-		const res = joinFunction([left, cc1] , [right, cc2])
-		const valueForKey = res[0]
-
-      if (!valueForKey.isBottom()) {
-        resM.state.set(key, valueForKey)
-      }
-	}
-		
-    return [resM, resCC]
+		return [resM, resCC]
 	}
 
 }
