0b25152b2c7e778cb61ac444ae6bfe7a5b90e0a8
guykhazma <Guy.Khazma@ibm.com>
2021-07-06 14:52:43 +0300

    more refactor

---
+++ benchmarks/array_update_index.js
@@ -0,0 +1,26 @@
+const { runTest } = require('./runBenchmark')
+
+function test(lib, doc1, n) {
+    doc1 = lib.change(doc1, "test", doc => {
+        doc.a = [ -1 ]
+    })
+    for (i=0; i<n; i++) {
+        doc1 = lib.change(doc1, "test", doc => {
+            doc.a[0] = i
+        })
+    }
+
+    return doc1 
+}
+
+function yjsTest(doc1, n) {
+    const yarray = doc1.getArray('a')
+    yarray.insert(0, [-1])
+    for (i=0; i<n; i++) {
+        yarray.delete(0,1)
+        yarray.insert(0, [i])
+    }
+    return doc1
+}
+
+runTest(test, yjsTest)
